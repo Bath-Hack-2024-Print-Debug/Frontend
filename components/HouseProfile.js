143e5f084cd096561ab39631a8530eb6632ff08e
WaterSpooner <max@the-spooners.co.uk>
2024-04-14 04:02:52 +0100

    merged main

---
+++ components/HouseProfile.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { Image, StyleSheet, Text, ScrollView, TouchableOpacity, Modal } from 'react-native';
+
+import { Images } from '../config';
+import { View } from './View';
+import { Colors } from "../config";
+import { FullHouse } from './FullHouse';
+
+export const HouseProfile = ({imageSource, price, address,desc}) => {
+
+  const [modalVisible, setModalVisible] = React.useState(true);
+  const changeModalVisible = () => {
+    setModalVisible(!modalVisible); // Correct, update state in an event handler
+  };
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.button} onPress = {changeModalVisible}>
+                <Image source={imageSource} style={styles.image} />
+                <View style = {styles.buttonTextContainer}>
+                    <Text style={styles.buttonText}>Address: {address}</Text>
+                    <Text style={styles.buttonText}>Price per Month: £{price}</Text>
+                </View>
+            </TouchableOpacity>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                        <FullHouse 
+                          imageSource = {imageSource} 
+                          address = {address} 
+                          propertyDes = {desc}
+                        />
+                        <TouchableOpacity onPress={changeModalVisible}>
+                            <Text style={styles.modalCloseButton}>Close</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'column', // Arrange children horizontally
+    flex: 1,
+    backgroundColour: Colors.lightGreen,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderWidth: 4,
+    borderColor: Colors.cream,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: Colors.darkGreen,
+  },
+  button: {
+    backgroundColor: Colors.darkGreen,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent:'space-between',
+    width: '90%',
+    padding: 10,
+  },
+  buttonTextContainer: {
+    flex: 1,
+    paddingLeft: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: Colors.lightGreen,
+    marginBottom: 10,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // semi-transparent background
+  },
+  modalContent: {
+      backgroundColor: Colors.lightGreen,
+      padding: 20,
+      borderRadius: 10,
+      elevation: 5, // shadow on Android
+  },
+  modalText: {
+      fontSize: 18,
+      marginBottom: 20,
+  },
+  modalCloseButton: {
+      fontSize: 16,
+      color: 'blue',
+      alignContent: 'center',
+      justifyContent: 'center',
+      textAlign: 'center',
+
+  },
+});
--- screens/HouseScreen.js
+++ screens/HouseScreen.js
@@ -2,63 +2,22 @@ import React from "react";
 import { StyleSheet, Text, ScrollView, Modal,Button, TouchableOpacity } from "react-native";
 import {View} from "../components";
 import { Colors } from "../config";
-import Card from "../components/Card";
 import SettingsIcon from "../components/settings";
-import { getSimilarStudents } from "../api/user";
 import { useState,useEffect } from "react";
-import UserDetail from "../components/UserDetail";
-import UserPreferences from "../components/UserPreferences";
+import { HouseProfile } from "../components/HouseProfile";
+
 
 export const HouseScreen = () => {
-  const [data, setData] = useState(null); // State to store fetched data
-
-  useEffect(() => {
-    const fetchData = async () => {
-        const response = await getSimilarStudents(); // Replace with your API endpoint
-        setData(response); // Update state with fetched data
-    };
-    fetchData(); // Call fetchData function when component mounts
-  }, []);
-
-  const [preferences,setPreferences] = useState(false);
-  const [modalVisible, setModalVisible] = useState(false);
-  const changeModalVisible = () => {
-    setModalVisible(!modalVisible); // Correct, update state in an event handler
-  };
-  const changepref = () => {
-    setPreferences(!preferences); // Correct, update state in an event handler
-  };
   return (
     <View style={styles.container}>
-      <View style={styles.topbar}>
-        <Text style={styles.text}>House Finder</Text>
-        <TouchableOpacity onPress={changeModalVisible}><SettingsIcon></SettingsIcon></TouchableOpacity>
-      </View>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}>
-          <View style={styles.buttonContainer}>
-          <Button title = "Account Details" onPress={changepref} disabled={preferences}></Button>
-          <Button title = "User Preferences" onPress={changepref} disabled={!preferences}></Button>
-          </View>
-          {preferences ? <UserDetail/> : <UserPreferences/>}
-          <View style={{backgroundColor:Colors.darkGray, paddingBottom:70}}>
-          <Button title = "close" onPress={changeModalVisible} ></Button>
-          </View> 
-        </Modal>
-      <ScrollView style={styles.cardContainer}>
-      {data && data.users && data.users.map((user, index) => {
-          {console.log(user);}
-          return <Card title={user.name} description={user.description} imageSource={require("../assets/PersonOne.jpeg")}></Card>
-        })}
-      </ScrollView>
+      <Text style={styles.text}>Potential houses</Text>
+      <HouseProfile imageSource={require("../assets/Person2.jpeg")} price={1000} address={"1 BATH RD"} desc = {""}></HouseProfile>
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
     paddingTop: 70,
     flex: 1,
     backgroundColor:Colors.lightGreen,
@@ -92,4 +51,12 @@ const styles = StyleSheet.create({
     maxHeight:100,
     fontSize:50,
   },
-});
+  buttonContainer:{
+    backgroundColor:Colors.darkGreen, 
+    paddingTop:60 ,
+    flex:1,
+    flexDirection:'row',
+    justifyContent:'center',
+    maxHeight:100,
+    fontSize:50,
+  }});
